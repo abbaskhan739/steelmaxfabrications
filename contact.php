b870d09de8a021ec1504ccff31b77c6ea9c5adf1
Umar Qureshi <dqureshiumar@gmail.com>
2021-01-27 00:20:16 +0530

    Contact Form Updated

--- contact.php
+++ contact.php
@@ -21,13 +21,13 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'dqureshiumar@gmail.com';                     // SMTP username
-    $mail->Password   = 'Siddika@2901';                               // SMTP password
+    $mail->Username   = 'steelmaxfabrication@gmail.com';                     // SMTP username
+    $mail->Password   = '(abcd)#1234#';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('noreply@steelmaxfabrications.com', 'SteelMax Fabrications');
+    $mail->setFrom('steelmaxfabrication@gmail.com', 'SteelMax Fabrications');
     $mail->addAddress('dqureshiumar@gmail.com', 'SteelMax Fabrications');     // Add a recipient
     //$mail->addAddress('ellen@example.com');               // Name is optional
     $mail->addReplyTo('info@steelmaxfabrications.com', 'SteelMax Fabrications');
