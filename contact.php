c2e22176e65e8f736f17590c933b167f616ae375
Umar Qureshi <dqureshiumar@gmail.com>
2021-02-01 00:37:33 +0530

    Code Updated dated 1st feb 2020

--- contact.php
+++ contact.php
@@ -18,7 +18,7 @@ $message = strip_tags($_POST['message']);
 try {
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
+    //$mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'steelmaxfabrication@gmail.com';                     // SMTP username
